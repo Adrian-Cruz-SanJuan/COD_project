d5fa5cb808db8c6a6832703b84b074957efd7007
Adrian-Cruz-SanJuan <50605282+Adrian-Cruz-SanJuan@users.noreply.github.com>
2019-11-12 09:55:39 -0600

    Actualizacion de la tabla usuario
    Se agregó el campo de fecha de nacimiento en la tabla del usuario

--- Yuko/hoja.php
+++ Yuko/hoja.php
@@ -15,7 +15,36 @@ $dia = $_POST['Dia'];
 $mes = $_POST['Mes'];
 $year = $_POST['Year'];
 
-$consulta = "CALL IO_K_C('$nombre','$apellidos','$correo','$password');";
+if($mes == 'Enero'){
+    $g = '01';
+}elseif ($mes == 'Febrero'){
+    $g = '02';
+}elseif ($mes == 'Marzo'){
+    $g = '03';
+}elseif ($mes == 'Abril'){
+    $g = '04';
+}elseif ($mes == 'Mayo'){
+    $g = '05';
+}elseif ($mes == 'Junio'){
+    $g = '06';
+}elseif ($mes == 'Julio'){
+    $g = '07';
+}elseif ($mes == 'Agosto'){
+    $g = '08';
+}elseif ($mes == 'Septiembre'){
+    $g = '09';
+}elseif ($mes == 'Octubre'){
+    $g = '10';
+}elseif ($mes == 'Noviembre'){
+    $g = '11';
+}elseif ($mes == 'Diciembre'){
+    $g = '12';
+}
+
+$fecha = $year.'-'.$g.'-'.$dia;
+
+
+$consulta = "CALL IO_K_C('$nombre','$apellidos','$correo','$password','$fecha');";
 $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta verificala");
 echo $nombre;
 
