012981337c11038a269f2eee7deb3014f13e3789
Adrian-Cruz-SanJuan <50605282+Adrian-Cruz-SanJuan@users.noreply.github.com>
2019-11-17 19:36:36 -0600

    Inserción de coordenadas

--- Yuko/hoja.php
+++ Yuko/hoja.php
@@ -1,7 +1,7 @@
 <?php
 $usuario = "root";
 $contrasena = "utec";
-$servidor = "localhost";
+$servidor = "localhost:3306";
 $basededatos = "COSECHANDO";
 
 $conexion = mysqli_connect( $servidor, $usuario,$contrasena) or die ("No se ha podido conectar al servidor de Base de datos");
@@ -14,25 +14,6 @@ $password = $_POST['Password'];
 $dia = $_POST['Dia'];
 $mes = $_POST['Mes'];
 $year = $_POST['Year'];
-
-if($nombre == "" or $apellidos == "" or $correo == "" or $password == ""){
-    echo '<script type="text/javascript">
-    alert("Todos los campos deben estar llenos");
-    window.location.href="sing_in.html";
-    </script>';
-}
-
-if($dia == 'Dia'){
-    echo '<script type="text/javascript">
-    alert("Todos los campos deben estar llenos");
-    window.location.href="sing_in.html";
-    </script>';
-}elseif($year == 'Año'){
-    echo '<script type="text/javascript">
-    alert("Todos los campos deben estar llenos");
-    window.location.href="sing_in.html";
-    </script>';
-}
 
 if($mes == 'Enero'){
     $g = '01';
@@ -68,10 +49,13 @@ if($mes == 'Enero'){
 $fecha = $year.'-'.$g.'-'.$dia;
 
 
+$consulta = "CALL Creacion_id_usuario('$nombre','$apellidos','$correo',AES_ENCRYPT('$password','gato'),'$fecha');";
+$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta verificala2");
 
-$consulta = "CALL IO_K_C('$nombre','$apellidos','$correo',AES_ENCRYPT('$password','gato'),'$fecha');";
-$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta verificala");
+/*$consulta_id = "SELECT USUARIO_ID FROM USUARIO WHERE CORREO_U = '$correo' AND PASSWORD_U = '$password';";
+$resultadoid = mysqli_query( $conexion, $consulta_id ) or die ( "Algo ha ido mal en la consulta verificala1");*/
 
+//echo $resultadoid;
 header("Location:address_register.html");
 
 mysqli_close( $conexion );
--- Yuko/ubicacion.php
+++ Yuko/ubicacion.php
@@ -1,35 +1,21 @@
 <?php
-$usuario = "root";
-$contrasena = "utec";
-$servidor = "localhost";
-$basededatos = "COSECHANDO";
+    $usuario = "root";
+    $contrasena = "utec";
+    $servidor = "localhost:3306";
+    $basededatos = "COSECHANDO";
 
-$conexion = mysqli_connect( $servidor, $usuario,$contrasena) or die ("No se ha podido conectar al servidor de Base de datos");
-$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues siempre no quizo conectar a la base de datos" );
+    $conexion = mysqli_connect( $servidor, $usuario,$contrasena) or die ("No se ha podido conectar al servidor de Base de datos");
+    $db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues siempre no quizo conectar a la base de datos" );
 
+    $coordenadas = $_POST['lol'];
+    $usuario = "user0";
+    $tama = "ldldlld";
+            
+    $consulta = "CALL Creacion_id_terreno('$coordenadas','$tama','$usuario');";
+    $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta verificala yuko");
 
-echo '<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v3"></script>
-<div id="map"></div>
-<script>
-    function findMe(){
-        var output = document.getElementById('map');
-        function localizacion(posicion){
-            var latitude = posicion.coords.latitude;
-            var longitude = posicion.coords.longitude;
-            output.innerHTML = latitude;
-            output.innerHTML = longitude;    
-        }
-        function error(){
-            output.innerHTML = "<p>No se pudo obtener tu ubicación</p>";
-        }
-        navigator.geolocation.getCurrentPosition(localizacion,error);
-    }
-</script>'
+    header("Location:main_user.html");
 
-$consulta = "INSERT INTO TERRENO";
-$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta verificala");
-
-header("Location:address_register.html");
-
-mysqli_close( $conexion );
-?>  
+    mysqli_close( $conexion );
+?>
+  
