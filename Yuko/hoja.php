a07c0cafdb35708046dd588e780a1b349351730e
Adrian-Cruz-SanJuan <50605282+Adrian-Cruz-SanJuan@users.noreply.github.com>
2019-11-22 13:29:30 -0600

    Login arreglado
    Lo siento Adolfo, la había cagado :'v

--- Yuko/hoja.php
+++ Yuko/hoja.php
@@ -1,6 +1,6 @@
 <?php
 $usuario = "root";
-$contrasena = "";
+$contrasena = "utec";
 $servidor = "localhost:3306";
 $basededatos = "COSECHANDO";
 
@@ -14,13 +14,10 @@ $password = $_POST['Password'];
 $fecha = $_POST['Fecha'];
 
 
-$consulta = "CALL Creacion_id_usuario('$nombre','$apellidos','$correo','$password','$fecha');";
+$consulta = "CALL Creacion_id_usuario('$nombre','$apellidos','$correo',MD5('$password'),'$fecha');";
 $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta verificala2");
 
-/*$consulta_id = "SELECT USUARIO_ID FROM USUARIO WHERE CORREO_U = '$correo' AND PASSWORD_U = '$password';";
-$resultadoid = mysqli_query( $conexion, $consulta_id ) or die ( "Algo ha ido mal en la consulta verificala1");*/
 
-//echo $resultadoid;
 header("Location:address_register.html");
 
 mysqli_close( $conexion );
