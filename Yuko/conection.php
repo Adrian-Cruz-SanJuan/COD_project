e849dc74dd1c4c820c19b6d0061879363fa6b9da
Adrian-Cruz-SanJuan <1718111017@utectulancingo.edu.mx>
2019-11-24 21:39:10 -0600

    insercion de usuario en terreno

--- Yuko/conection.php
+++ Yuko/conection.php
@@ -15,5 +15,10 @@ $consulta="SELECT * FROM USUARIO WHERE USUARIO_ID='$varsession';";
 $resultado=mysqli_query($conexion, $consulta);
 
 $indice = $resultado->fetch_row();
+$id_usuario = $indice[0];
+$nombre_usuario = $indice[1];
+$apellidos_usuario = $indice[2];
+$correo_usuario = $indice[3];
+$fecha_nac_usuario = $indice[5];
 
 mysqli_close($conexion);
